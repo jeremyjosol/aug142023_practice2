c4c50fcaabb2e984fc3e654b7bbf99ef31ea555e
Jeremy Josol <jeremyjosol@gmail.com>
2023-08-14 16:31:22 -0700

    Debug input values, refer back to string inputs
    Co-authored-by: Richard Barbour <richyb07@gmail.com>

--- scripts.js
+++ scripts.js
@@ -2,6 +2,14 @@ function checksSubmission(event){
   event.preventDefault();
   const checkedItems = document.querySelectorAll("input:checked");
   let checkedItemsArray = Array.from(checkedItems);
+  checkedItemsArray.sort();
+
+checkedItemsArray.forEach(function(item) {
+  let arrayOfValues = [];
+  arrayOfValues.push(item.value);
+  arrayOfValues.sort();
+  console.log("This is the arrayOfValues in alpha-order: " + arrayOfValues);
+});
 
 checkedItemsArray.forEach(function(item){
   const pTag = document.createElement("p");
